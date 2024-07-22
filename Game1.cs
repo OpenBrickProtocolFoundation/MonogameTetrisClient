318587cc5882b4d33316439ed27cf9af7457dda5
Michael Gerhold <michael.gerhold@web.de>
2024-07-22 22:10:26 +0200

    change formatting

--- Game1.cs
+++ Game1.cs
@@ -31,8 +31,8 @@ public class Game1 : Game {
     }
 
     protected override void Update(GameTime gameTime) {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
-            Keyboard.GetState().IsKeyDown(Keys.Escape)) {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed
+            || Keyboard.GetState().IsKeyDown(Keys.Escape)) {
             Exit();
         }
 
--- Program.cs
+++ Program.cs
@@ -1,3 +1,2 @@
-﻿
-using var game = new MonogameTetrisClient.Game1();
+﻿using var game = new MonogameTetrisClient.Game1();
 game.Run();
